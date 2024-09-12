18277dd308835ec0e9698403891cc10df035c067
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-09-12 17:46:02 +0100

    movilarte header

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecione o elemento pai do dropdown
+    var dropdown = document.querySelector('.dropdown');
+    var dropdownMenu = document.querySelector('.dropdown-menu');
+  
+    // Quando o mouse passar sobre o dropdown
+    dropdown.addEventListener('mouseover', function() {
+        dropdownMenu.classList.add('show');
+    });
+  
+    // Quando o mouse sair do dropdown
+    dropdown.addEventListener('mouseout', function() {
+        dropdownMenu.classList.remove('show');
+    });
+});
