5095695a58c853f219cc1d0fbcc67b51264c64f6
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-10-16 14:42:11 +0100

    db_call

--- helpers/base_de_dados_helpers.php
+++ helpers/base_de_dados_helpers.php
@@ -8,9 +8,36 @@ $pass = "";
 $conexao = new PDO("mysql:host=$host;dbname=$dbname; charset=utf8mb4;", $user, $pass);
 
 
-function selectSQL($sql){
+function selectSQL($sql, $params=[]){
     global $conexao;
-    $consulta = $conexao->query($sql);
+    $consulta = $conexao->prepare($sql);
+
+    $consulta->execute($params);
+
     $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
     return $resultado;
+}
+
+function selectSQLUnico($sql, $params=[]){
+    global $conexao;
+
+    $consulta = $conexao->prepare($sql);
+
+    $consulta->execute($params);
+
+    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+
+    return $resultado;
+}
+
+function iduSQL($sql, $params = []) {
+    global $conexao;
+
+    $consulta = $conexao->prepare($sql);
+
+    $sucesso = $consulta->execute($params);
+
+    return $sucesso;
 }
