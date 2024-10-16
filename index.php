f505bb8be56986149810650b6edd539c71db6800
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-10-16 16:24:57 +0100

    index_php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 
+
+require_once("requisicoes.php");
+
 require("componentes/header.php");
 require("views/index_view.php");
 require("componentes/footer.php");
---
+++ requisicoes.php
@@ -0,0 +1,15 @@
+<?php
+
+
+require_once("helpers/globais_helper.php");
+
+require_once("helpers/base_de_dados_helper.php");
+require_once("helpers/carousel.php");
+require_once("helpers/sofas.php");
+require_once("helpers/salas.php");
+require_once("helpers/quartos.php");
+require_once("helpers/diversos.php");
+require_once("helpers/decoracao.php");
+require_once("helpers/moveistv.php");
+require_once("helpers/home.php");
+require_once("helpers/contactos.php");
