fb5015ec237f36dea4cbc22266c8609a828c2f8b
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-10-18 14:53:40 +0100

    atualizacao

--- views/index_view.php
+++ views/index_view.php
@@ -36,37 +36,37 @@
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 salas" id="salas">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Salas</a>
+                    <a class="pointer fs-3 pt-5" href="salas.php">Salas</a>
                 </div>
             </div>
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 sofas">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Sofás</a>
+                    <a class="pointer fs-3 pt-5" href="sofas.php">Sofás</a>
                 </div>
             </div>
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 decoracao">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Decoração</a>
+                    <a class="pointer fs-3 pt-5" href="decoracoes.php">Decoração</a>
                 </div>
             </div>
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 quartos">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Quartos</a>
+                    <a class="pointer fs-3 pt-5" href="quartos.php">Quartos</a>
                 </div>
             </div>
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 diversos">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Diversos</a>
+                    <a class="pointer fs-3 pt-5" href="diversos.php">Diversos</a>
                 </div>
             </div>
 
             <div class="col-8 col-sm-5 col-lg-3 my-2 artigos_height back_image px-0 moveis_tv">
                 <div class="effect">
-                    <a class="pointer fs-3 pt-5" href="#">Móveis de Tv</a>
+                    <a class="pointer fs-3 pt-5" href="moveis_tv.php">Móveis de Tv</a>
                 </div>
             </div>
             
