896527462506604f1d01055753573cf77a532e3e
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-11-13 17:15:51 +0000

    css

--- backoffice/views/carousel_editar_view.php
+++ backoffice/views/carousel_editar_view.php
@@ -24,14 +24,11 @@ if($form){
 
 ?>
 
-    <script>
-        tinymce.init({
-            selector: 'textarea'
-        });
-    </script>
+    
 
 
-    <main class="container">
+
+    <!-- <main class="container">
 
         <div class="row">
             <h1 class="text-center col-12">
@@ -50,7 +47,7 @@ if($form){
                 <input type="text" name="img_carousel" required class="w-50" value="<?= $ce["img_carousel"] ?>">
                 <br><br>
 
-                <a class="gestor" target="_blank" href="<?= $url_base; ?>backoffice/tinyfilemanager/tinyfilemanager.php">
+                <a class="gestor" target="_blank" href="<?= $url_base; ?>backoffice/richfilemanager/connectors/php/filemanager.php">
                     Gestor de Ficheiros
                 </a>
 
@@ -62,4 +59,50 @@ if($form){
 
         </div>
 
+    </main> -->
+
+
+
+
+
+
+    <!-- Div onde o RichFilemanager será carregado -->
+    <div id="richfilemanager"></div>
+
+    <!-- Script do RichFilemanager -->
+    <script src="backoffice/richfilemanager/dist/richfilemanager.min.js"></script>
+    <script>
+        $(function () {
+            $('#richfilemanager').richFilemanager({
+                // URL do conector PHP configurado
+                apiConnectorUrl: 'backoffice/richfilemanager/connectors/php/filemanager.php'
+            });
+        });
+    </script>
+
+    <!-- Conteúdo principal da página -->
+    <main class="container">
+        <div class="row">
+            <h1 class="text-center col-12">Carousel - Editar</h1>
+        </div>
+
+        <div class="row mt-5">
+            <form class="col-12">
+                <!-- Campo oculto para o ID -->
+                <input type="hidden" name="id" value="<?= htmlspecialchars($id); ?>">
+
+                <br>
+
+                <label for="img_carousel">Imagem</label>
+                <input type="text" name="img_carousel" required class="w-50" value="<?= htmlspecialchars($ce["img_carousel"]); ?>">
+                <br><br>
+
+                <!-- O link para abrir o gestor de ficheiros foi removido porque ele já está embutido na página -->
+
+                <br><br>
+
+                <input type="submit" value="Editar">
+            </form>
+        </div>
     </main>
+
